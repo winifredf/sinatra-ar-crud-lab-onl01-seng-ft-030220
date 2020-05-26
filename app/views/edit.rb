8ecf1cfd9fe367d4242dc07ba45fdf0adc4bf5a6
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-26 10:05:11 +0000

    Automatically backed up by Learn

--- app/views/edit.rb
+++
@@ -1,7 +0,0 @@
-<h2><%= @articles.title %></h2><br>
-<h2><%= @articles.content %></h2><br>
-
-<form methods="POST" action="/articles/<%= @articles.id %">
-  <input id="hidden" type="hidden" name="_method" value="DELETE">
-  <input type="submit" value="delete">
-</form>
