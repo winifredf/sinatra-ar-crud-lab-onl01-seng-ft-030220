96caf163875d08a9fa7a5617c6f6d4ed218af721
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-26 06:53:58 +0000

    Automatically backed up by Learn

--- app/views/new.rb
+++ app/views/new.rb
@@ -1,6 +1,6 @@
 <form action="/articles" method="POST">
 
-<label for="title">Title:</label><br>
+<label for="title">Title:</label>
 <input id="title" type="text" name="title">
 <label for="article">Article:</label>
 <input id="content" type="text" name="content">
