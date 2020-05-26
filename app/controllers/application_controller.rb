02165b245960499f80d5655cd057310d12a5bdb7
Winifred Frierson <winifred.dykes@hotmail.com>
2020-05-26 10:15:59 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
-
 require_relative '../../config/environment'
 
-class ApplicationController < Sinatra::Base
+class ArticlesController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
@@ -9,5 +8,43 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    
+  end
+  
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post 'articles' do
+    @articles = Articles.create(params)
+    redirect to "/articles/#{articles.id}"
+  end
+  
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+  
+  get 'articles/:id' do
+    @articles = Article.find_(params[:id])
+    erb :show
+  end
+  
+  get '/articles/:id/edit' do
+    @articles = Article.find(params[:id])
+    erb :edit
+  
+  patch "/articles/:id" do
+    @articles = Article.find(params[:id])
+    params.delete("_method")
+    if @articles.update(params)
+      redirect to "/articles/#{@articles.id}"
+    else redirect to "/articles/#{articles.id}/edit"
+    end
+  end
+  
+  delete "/articles/:id" do
+    Article.destroy(params[:id])
+    redirect to "/articles"
   end
 end
---
+++ app/views/new.rb
@@ -0,0 +1,9 @@
+<form method="POST" action="articles">
+
+  <label for="title">Title:</label>
+  <input id="title" type="text" name="title">
+  <label for="article">Article:</label>
+  <input id="content" type="text" name="content">
+  <input type="submit" value="submit">
+
+</form>
